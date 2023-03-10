f28cc5d21392a75858a48a38353f2bb5a70e9fde
Minung Han <hmu332233@gmail.com>
2023-03-11 01:06:29 +0900

    issue 테스트용 코드 작성

--- packages/action.issue-to-json/src/index.ts
+++ packages/action.issue-to-json/src/index.ts
@@ -1,3 +1,22 @@
-const text: string = 'text';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-console.log(`Hello!`, text);
+function getErrorMessage(error: unknown) {
+  if (error instanceof Error) return error.message;
+  return String(error);
+}
+
+async function run(): Promise<void> {
+  try {
+    const context = github.context;
+
+    if (context.payload.issue) {
+      const issueNumber = context.payload.issue.number;
+      core.info(`Issue #${issueNumber} opened`);
+    }
+  } catch (error) {
+    core.setFailed(getErrorMessage(error));
+  }
+}
+
+run();
