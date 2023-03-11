5e871ffd6de6dd39a4e22ef57db0eadf1ef487b5
Minung Han <hmu332233@gmail.com>
2023-03-11 16:32:16 +0900

    git config에 bot 이름 설정

--- packages/action.issue-to-json/src/utils/git.ts
+++ packages/action.issue-to-json/src/utils/git.ts
@@ -1,8 +1,8 @@
 import { exec } from 'shelljs';
 
 export function commitAndPush(path: string, message: string) {
-  // exec(`git config user.email "bot@minung.dev"`);
-  // exec(`git config user.name "Bot"`);
+  exec(`git config user.email "bot@minung.dev"`);
+  exec(`git config user.name "Bot"`);
   exec(`git add ${path}`);
   exec(`git commit -m "${message}"`);
   exec(`git push`);
