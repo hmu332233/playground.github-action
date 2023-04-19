067ae7ddc598167c22a6fd955cc54ba2ee248fe0
Minung Han <hmu332233@gmail.com>
2023-04-20 04:23:01 +0900

    명령어 수정

--- packages/action.compare-packages/src/index.ts
+++ packages/action.compare-packages/src/index.ts
@@ -36,12 +36,12 @@ exec('git symbolic-ref --short HEAD');
 // 추가된 패키지 목록 출력
 const result2 = exec(
   // `git diff --name-only ${COMPARE_TARGET_BRANCH} ${targetSha} | grep package.json | xargs cat | jq -r '.dependencies | keys[]' | paste -sd ", "`,
-  `git diff ${COMPARE_TARGET_BRANCH} origin/${github.context.payload.pull_request?.head.ref} -- package.json | grep -E '^+' | grep -E '\\".+\\":\\s*\\".+\"' | sed -E 's/^.*\\"([^"]+)\\":.*$/\\1/' | tr '\\n' ',' | sed 's/,$//'`,
+  `git diff ${COMPARE_TARGET_BRANCH} origin/${github.context.payload.pull_request?.head.ref} -- package.json | grep -E '^\\+' | grep -E '\".+\":\\s*\".+"' | sed -E 's/^\\+.*\"([^"]+)\":.*$/\\1/' | tr '\\n' ',' | sed 's/,$//'`,
   // { silent: true },
 );
 
 console.log(
-  `git diff ${COMPARE_TARGET_BRANCH} origin/${github.context.payload.pull_request?.head.ref} -- package.json | grep -E '^+' | grep -E '\\".+\\":\\s*\\".+\"' | sed -E 's/^.*\\"([^"]+)\\":.*$/\\1/' | tr '\\n' ',' | sed 's/,$//'`,
+  `git diff ${COMPARE_TARGET_BRANCH} origin/${github.context.payload.pull_request?.head.ref} -- package.json | grep -E '^\\+' | grep -E '\".+\":\\s*\".+"' | sed -E 's/^\\+.*\"([^"]+)\":.*$/\\1/' | tr '\\n' ',' | sed 's/,$//'`,
 );
 
 if (result2.code !== 0) {
