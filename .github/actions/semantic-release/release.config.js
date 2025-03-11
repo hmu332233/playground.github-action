eccee22e9b96cf7200a164653a70bdd4a8142bd5
Minung Han <hmu332233@gmail.com>
2025-03-11 19:17:36 +0900

    feat: 시맨틱 릴리즈 action 추가

---
+++ .github/actions/semantic-release/release.config.js
@@ -0,0 +1,18 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ['custom/main'],
+  plugins: [
+    "@semantic-release/commit-analyzer",        // 커밋 메시지 분석
+    "@semantic-release/release-notes-generator", // 릴리스 노트 생성
+    "@semantic-release/changelog",              // CHANGELOG.md 생성
+    [
+      "@semantic-release/git",
+      {
+        "message": "chore(release): ${nextRelease.version}"
+      }
+    ],
+  ],
+  tagFormat: 'custom.v${version}',
+};
